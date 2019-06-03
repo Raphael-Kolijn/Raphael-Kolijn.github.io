1e1b89adfa83987f512ecc6c66af3fb55b4015ab
Maurits Maas <367868@student.fontys.nl>
2019-06-03 15:50:43 +0200

    fix

---
+++ precache-manifest.2660d73a4d05771b4cd06717987da4b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b372dd964006189d9ced",
+    "url": "/js/chunk-vendors.e84efab4.js"
+  },
+  {
+    "revision": "cf4817d5a5dcc664be1c",
+    "url": "/js/app.b7ae54a9.js"
+  },
+  {
+    "revision": "7bc9b26b19c85fee3ed8e6191510bffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01240027e48854a576bd6f3deadf3910",
+    "url": "/img/politie-logo.01240027.png"
+  },
+  {
+    "revision": "9a19a5a8dbb8bda377848dc4249ec9d3",
+    "url": "/img/logo.9a19a5a8.png"
+  },
+  {
+    "revision": "b372dd964006189d9ced",
+    "url": "/css/chunk-vendors.e24c7c84.css"
+  },
+  {
+    "revision": "cf4817d5a5dcc664be1c",
+    "url": "/css/app.c331e2e3.css"
+  }
+];
